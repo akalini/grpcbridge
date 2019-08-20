78ca99d6b3e2ae9b5ca7df930bb81706ddbfe3a6
Sam <47120977+sam-step@users.noreply.github.com>
2019-08-20 15:41:35 -0700

    Fix swagger generation for map fields (#28)

--- swagger/src/main/java/grpcbridge/swagger/model/SwaggerModel.java
+++ swagger/src/main/java/grpcbridge/swagger/model/SwaggerModel.java
@@ -1,5 +1,7 @@
 package grpcbridge.swagger.model;
 
+import static java.util.Collections.emptyMap;
+
 import grpcbridge.swagger.model.Property.Type;
 import java.util.LinkedList;
 import java.util.List;
@@ -13,18 +15,20 @@ public class SwaggerModel {
     private final Type type;
     private final Map<String, Property> properties;
     private final List<String> required = new LinkedList<>();
+    private final Property additionalProperties;
 
-    SwaggerModel(Type type, Map<String, Property> properties) {
+    SwaggerModel(Type type, Map<String, Property> properties, Property additionalProperties) {
         this.type = type;
         this.properties = properties;
+        this.additionalProperties = additionalProperties;
     }
 
     public static SwaggerModel forMessage() {
-        return new SwaggerModel(Type.OBJECT, new TreeMap<>());
+        return new SwaggerModel(Type.OBJECT, new TreeMap<>(), null);
     }
 
     public static SwaggerModel empty() {
-        return new SwaggerModel(Type.ARRAY, new TreeMap<>());
+        return new SwaggerModel(Type.ARRAY, new TreeMap<>(), null);
     }
 
     public Property getProperty(String name) {
@@ -45,5 +49,27 @@ public class SwaggerModel {
 
     public boolean hasProperties() {
         return properties.size() > 0;
+    }
+
+    public SwaggerModel asMapDefinition() {
+        if (type != Type.OBJECT) {
+            throw new IllegalArgumentException("Non-object model is not a map: " + type);
+        }
+
+        Property key = properties.get("key");
+        Property value = properties.get("value");
+        if (key == null || value == null) {
+            throw new IllegalArgumentException("Model is not a map entry, must have key/value");
+        }
+        if (key.type != Type.STRING) {
+            throw new IllegalArgumentException("Non-string map key not supported: " + key.type);
+        }
+        if (properties.size() != 2) {
+            throw new IllegalArgumentException(
+                "Model is not a map entry, must have exactly two fields"
+            );
+        }
+
+        return new SwaggerModel(Type.OBJECT, emptyMap(), value);
     }
 }
